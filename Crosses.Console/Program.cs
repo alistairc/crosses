edba99527290aaf311d2a0fb6545c2c99b37bd8a
Alistair Cooke <alistair.cooke77@gmail.com>
2018-09-09 21:05:46 +0100

    Initial projects

---
+++ Crosses.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Crosses.Console
+{
+    using System;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
