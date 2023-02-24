6106bd7418ddeaf2531fba686c9bf6977449dca7
Alistair Cooke <alistair.cooke77@gmail.com>
2023-02-24 12:26:34 +0000

    Use immutable data structures

--- Crosses.Core/Board.cs
+++ Crosses.Core/Board.cs
@@ -1,21 +1,31 @@
+using System.Collections.Immutable;
+
 namespace Crosses.Core;
 
 public record Board
 {
-    SquareState[,] State { get; init; } = new SquareState[3, 3];
+    public static readonly Board Blank = new();
+
+    ImmutableArray<SquareState> NewState { get; init; } = ImmutableArray.CreateRange(Enumerable.Repeat(SquareState.Blank, 9));
 
     public SquareState StateAt(int x, int y)
     {
-        return State[x, y];
+        return NewState[IndexForCoord(x, y)];
     }
 
     public Board MoveAt(int x, int y, Player player)
     {
-        var newState = (SquareState[,])State.Clone();
-        newState[x, y] = StateFromPlayer(player);
-        return new Board { State = newState };
+        var index = IndexForCoord(x, y);
+        var mutable = NewState.ToBuilder();
+        mutable[index] = StateFromPlayer(player);
+        return new Board { NewState = mutable.ToImmutable() };
     }
 
+    static int IndexForCoord(int x, int y)
+    {
+        return x + y * 3;
+    }
+    
     static SquareState StateFromPlayer(Player player)
     {
         return player switch
