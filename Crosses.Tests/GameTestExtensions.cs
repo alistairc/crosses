c1794a3c2229644c53ed06a52d37e8d43050ddd0
Alistair Cooke <alistair.cooke77@gmail.com>
2023-03-10 11:18:19 +0000

    Test for playing a whole game

---
+++ Crosses.Tests/GameTestExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Crosses.Core;
+
+namespace Crosses.Tests;
+
+static class GameTestExtensions
+{
+    public static Game Print(this Game game)
+    {
+        Console.WriteLine(game.Board);
+        Console.WriteLine($"Next player: {game.NextTurn}");
+        return game;
+    }
+}
---
+++ Crosses.Tests/GameplayTests.cs
@@ -0,0 +1,24 @@
+﻿using Crosses.Core;
+
+namespace Crosses.Tests;
+
+public class GameplayTests
+{
+    [Test]
+    public void PlayingACompleteGame()
+    {
+        var result = Game.Start().Print()
+                .MoveAt(0, 0).Print()
+                .MoveAt(2, 2).Print()
+                .MoveAt(2, 0).Print()
+                .MoveAt(1, 0).Print()
+                .MoveAt(0, 2).Print()
+                .MoveAt(0, 1).Print()
+                .MoveAt(1, 1).Print()
+            ;
+
+        //TODO: Need to implement winning
+        //result.GameState.ShouldBe(PlayerWon(Player.O));
+        // O should have won
+    }
+}
